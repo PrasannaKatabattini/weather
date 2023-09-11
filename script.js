59795da93e263361bf9eba49cc1eb7e59cc7428d
KATABATTINI LAKSHMI PRASANNA <127750897+PrasannaKatabattini@users.noreply.github.com>
2023-09-11 14:25:20 +0530

    script.js

--- script.js
+++ script.js
@@ -1,66 +1,89 @@
-const wrapper=document.querySelector(".wrapper"),
-inputPart=wrapper.querySelector(".input-part"),
-infoTxt=inputPart.querySelector(".info-txt"),
-inputField=inputPart.querySelector("input");
-locationBtn=inputPart.querySelector("button");
+const wrapper = document.querySelector(".wrapper"),
+inputPart = document.querySelector(".input-part"),
+infoTxt = inputPart.querySelector(".info-txt"),
+inputField = inputPart.querySelector("input"),
+locationBtn = inputPart.querySelector("button"),
+weatherPart = wrapper.querySelector(".weather-part"),
+Img= weatherPart.querySelector("img"),
+arrowBack = wrapper.querySelector("header i");
+
 let api;
-inputField.addEventListener("keyup",e =>{
-    if(e.key=="Enter" && inputField.value!=""){
+
+inputField.addEventListener("keyup", e =>{
+    if(e.key == "Enter" && inputField.value != ""){
         requestApi(inputField.value);
     }
 });
 
-
-locationBtn.addEventListener("click",()=>{
+locationBtn.addEventListener("click", () =>{
     if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(onSuccess,onError);
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
     }else{
         alert("Your browser not support geolocation api");
     }
 });
 
+function requestApi(city){
+    api="https://api.openweathermap.org/data/2.5/weather?units=metric&q="+city+"&appid=83ccb788f78694d572f631660edb7556";
+        fetchData();
+}
 
 function onSuccess(position){
-    const {latitude,longitude}=position.coords;
-    const laturl="http://api.openweathermap.org/geo/1.0/reverse?lat=";
-    const long_part="&lon=";
-    const end="&limit=5&appid=83ccb788f78694d572f631660edb7556";
-     fetchData();
+    const {latitude, longitude} = position.coords;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=83ccb788f78694d572f631660edb7556`;
+    fetchData();
 }
 
 function onError(error){
-    infoTxt.innerText=error.message;
+    infoTxt.innerText = error.message;
     infoTxt.classList.add("error");
 }
-function requestApi(city){
-     api="https://api.openweathermap.org/data/2.5/weather?units=metric&q=Guntur&appid=83ccb788f78694d572f631660edb7556";
-    fetchData();
-}
+
 function fetchData(){
-    infoTxt.innerText="Getting weather details....";
+    infoTxt.innerText = "Getting weather details...";
     infoTxt.classList.add("pending");
-    fetch(api).then(response=>response.json()).then(result=>weatherDetails(result));
+    fetch(api).then(res => res.json()).then(result => weatherDetails(result)).catch(() =>{
+        infoTxt.innerText = "Something went wrong";
+        infoTxt.classList.replace("pending", "error");
+    });
 }
+
 function weatherDetails(info){
-    infoTxt.classList.replace("pending","error");
-    if(info.cod=="404"){
-        infoTxt.innerText=`${inputField.value} is not a valid city name`;
+    if(info.cod == "404"){
+        infoTxt.classList.replace("pending", "error");
+        infoTxt.innerText = `${inputField.value} isn't a valid city name`;
     }else{
-        const city=info.name;
-        const country=info.sys.country;
-        const {description,id}=info.weather[0];
-        const {feels_like,humidity,temp} =info.main;
+        const city = info.name;
+        const country = info.sys.country;
+        const {description, id} = info.weather[0];
+        const {temp, feels_like, humidity} = info.main;
 
-        wrapper.querySelector(".temp .numb").innerText=temp;
-        wrapper.querySelector(".weather").innerText=description;
-        wrapper.querySelector(".location span").innerText=`${city},${country}`;
-        wrapper.querySelector(".temp .numb-2").innerText=feels_like;
-        wrapper.querySelector(".humidity span").innerText=humidity;
-
-        infoTxt.classList.remove("pending","error");
+        if(id == 800){
+            Img.src = "images/clear.png";
+        }else if(id >= 200 && id <= 232){
+            Img.src = "icons/mist.png";  
+        }else if(id >= 600 && id <= 622){
+            Img.src = "images/snow.png";
+        }else if(id >= 701 && id <= 781){
+            Img.src = "images/wind.png";
+        }else if(id >= 801 && id <= 804){
+            Img.src = "images/clouds.png";
+        }else if((id >= 500 && id <= 531) || (id >= 300 && id <= 321)){
+            Img.src = "images/rain.png";
+        }
+        
+        weatherPart.querySelector(".temp .numb").innerText = Math.floor(temp);
+        weatherPart.querySelector(".weather").innerText = description;
+        weatherPart.querySelector(".location span").innerText = `${city}, ${country}`;
+        weatherPart.querySelector(".temp .numb-2").innerText = Math.floor(feels_like);
+        weatherPart.querySelector(".humidity span").innerText = `${humidity}%`;
+        infoTxt.classList.remove("pending", "error");
+        infoTxt.innerText = "";
+        inputField.value = "";
         wrapper.classList.add("active");
-        console.log(info);
-
-
     }
 }
+
+arrowBack.addEventListener("click", ()=>{
+    wrapper.classList.remove("active");
+});
